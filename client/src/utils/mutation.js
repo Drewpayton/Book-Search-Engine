d4d5377cd7cdde557abffce0e5d2b44f4522ab34
Drew Payton <drewpayton18@gmail.com>
2022-08-30 07:44:36 -0500

    finishing up the mutations for the gql

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -11,3 +11,50 @@ export const LOGIN_USER = gql`
         }
     }
 `
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addProfile(username: $username, email: $email, password: $password)
+        token
+        user {
+            _id
+            username
+        }
+    }
+`
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($book: InputBook!) {
+        savebook(book: $book) {
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`
+
+export const REMOVE_BOOK = gql`
+    mutatuion removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            savedBook {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`
