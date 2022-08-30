6362ff17edd14d94372ccee4299528048fd1b2d3
Drew Payton <drewpayton18@gmail.com>
2022-08-29 19:42:06 -0500

    finsihed up the queries and started working on the mutations

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,13 @@
+import { gql } from 'apollo-server-express'
+
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+        loginUser(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`
